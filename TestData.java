9c131a0658b1693a46180df76b3bc03ab6eb22da
DevilSOF <soldiersof@gmail.com>
2019-10-11 20:23:09 +0500

    minor changes

--- TestData.java
+++ TestData.java
@@ -8,13 +8,16 @@ public class TestData {
   @Test
   public void main() throws Exception {
     Calculations testCalculations = new Calculations();
-    String[] csvFileNames = { "TestData_in.csv", "TestData_out.csv"};
+    String[] csvFileNames = { "TestData_in.csv", "TestData_out.csv" };
+    String charSet = "utf-8";
 
     testCalculations.main(csvFileNames);
 
     File inputFile = new File(csvFileNames[1]);
     File compareFile = new File("TestData_true_result.csv");
 
-    assertTrue("The files are differ!", FileUtils.contentEquals(inputFile, compareFile));
+    assertEquals("The files are differ!",
+                 FileUtils.readFileToString(inputFile, charSet),
+                 FileUtils.readFileToString(compareFile, charSet));
   }
 }
